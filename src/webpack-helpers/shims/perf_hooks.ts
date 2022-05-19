7ce45263b2cd909a3508b6bac3966273265652f5
Katsuyuki Omuro <komuro@fastly.com>
2022-05-19 19:58:58 +0900

    Fix origin time

--- src/webpack-helpers/shims/perf_hooks.ts
+++ src/webpack-helpers/shims/perf_hooks.ts
@@ -3,11 +3,24 @@
  * Licensed under the MIT license. See LICENSE file for details.
  */
 
-const origin = Date.now();
+let origin: number | null;
+addEventListener('fetch', () => {
+  origin = Date.now();
+});
 
 export const performance = {
-  timeOrigin: origin,
+  get timeOrigin() {
+    if(origin == null) {
+      console.warn('Read from performance.timeOrigin before fetch likely to give unexpected results.');
+      return 1;
+    }
+    return origin!;
+  },
   now() {
+    if(origin == null) {
+      console.warn('Read from performance.now before fetch event likely to give unexpected results.');
+      return 1;
+    }
     return Date.now() - origin;
   },
 };
