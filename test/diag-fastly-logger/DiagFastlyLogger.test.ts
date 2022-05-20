efa365351cbefa68e32a34ccb21f34474202786d
Katsuyuki Omuro <komuro@fastly.com>
2022-05-21 04:26:13 +0900

    Stub upcoming tests

---
+++ test/diag-fastly-logger/DiagFastlyLogger.test.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright Fastly, Inc.
+ * Licensed under the MIT license. See LICENSE file for details.
+ */
+
+import /* {DiagFastlyLogger} from */ "../../src/diag-fastly-logger";
+
+describe('DiagFastlyLogger', function() {
+  it('instance');
+});
---
+++ test/webpack-helpers/index.test.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright Fastly, Inc.
+ * Licensed under the MIT license. See LICENSE file for details.
+ */
+
+import /* * as webpackHelpers from */ "../../src/webpack-helpers";
+
+describe('Webpack Helpers', function() {
+  it('apply');
+});
